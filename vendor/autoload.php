380884ba2ec834c3cea6edff344fe53c1906596b
Muflone <webreg@vbsimple.net>
2016-10-23 18:54:37 +0200

    Updated grav to version 1.1.8

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit1440df0a01425ed0b23742052ad04ca4::getLoader();
+return ComposerAutoloaderInit7110dc61338383a6a240f84b058bcd0f::getLoader();
